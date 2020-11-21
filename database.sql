4564a5f6543194a066a4d20267de86992c6472b9
Milo Gilad <milo@milogilad.com>
2020-11-20 19:51:51 -0500

    No longer using ISBN as primary key; added constraints

--- database.sql
+++ database.sql
@@ -1,40 +1,41 @@
 CREATE TABLE publishers (
     publisher_id bigserial,
-    name varchar(100),
-    location varchar(100),
+    name varchar(100) NOT NULL,
+    location varchar(100) NOT NULL,
     CONSTRAINT publisher_key PRIMARY KEY (publisher_id)
 );
 
 CREATE TABLE authors (
     author_id bigserial,
-    first_name varchar(50),
-    last_name varchar(50),
-    gender varchar(20),
-    nationality varchar(50),
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    gender varchar(20) NOT NULL,
+    nationality varchar(50) NOT NULL,
     CONSTRAINT author_key PRIMARY KEY (author_id)
 );
 
 CREATE TABLE members (
     member_id bigserial,
-    first_name varchar(50),
-    last_name varchar(50),
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
     CONSTRAINT member_key PRIMARY KEY (member_id)
 );
 
 CREATE TABLE books (
+    book_id bigserial,
     ISBN varchar(50),
     publisher_id integer REFERENCES publishers (publisher_id),
     author_id integer REFERENCES authors (author_id),
-    book_name varchar(90),
+    book_name varchar(90) NOT NULL,
     book_genre varchar(50),
-    total_copies integer,
-    CONSTRAINT book_key PRIMARY KEY (ISBN)
+    total_copies integer CHECK (total_copies > 0),
+    CONSTRAINT book_key PRIMARY KEY (book_id)
 );
 
 CREATE TABLE borrow_records (
     member_id integer REFERENCES members (member_id),
-    ISBN varchar(50) REFERENCES books (ISBN),
-    return_by date
+    book_id varchar(50) REFERENCES books (book_id),
+    return_by date NOT NULL
 );
 
 INSERT INTO
@@ -72,6 +73,6 @@ VALUES
     );
 
 INSERT INTO
-    borrow_records (member_id, ISBN, return_by)
+    borrow_records (member_id, book_id, return_by)
 VALUES
-    (1, '978-3-16-148410-0', '2020-10-30');
+    (1, 1, '2020-10-30');
